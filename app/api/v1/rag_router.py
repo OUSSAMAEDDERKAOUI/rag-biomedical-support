ff94670ee3702e4bd561ac83911623aca0197ff5
OUSSAMAEDDERKAOUI <edderkaouioussama@gmail.com>
2026-02-06 09:41:31 +0100

    L'ajout de api end point ask .

--- app/api/v1/rag_router.py
+++ app/api/v1/rag_router.py
@@ -14,3 +14,16 @@ async def index_pdf(file: UploadFile = File(...)):
     result = build_pipeline(path)
 
     return result
+from fastapi import APIRouter
+from app.services.rag_service import ask_question
+
+router = APIRouter()
+
+@router.post("/ask")
+async def ask(data: dict):
+
+    question = data.get("question")
+
+    result = ask_question(question)
+
+    return result
